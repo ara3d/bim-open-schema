e8c8e355f25aba8e6470f6a481c117467460154f
Christopher Diggins <cdiggins@gmail.com>
2025-07-08 12:12:48 -0400

    Update schema.cs

--- schema.cs
+++ schema.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+using System;
 
 namespace BIMOpenSchema;
 
@@ -9,34 +9,54 @@ public enum LevelIndex : long { }
 public enum CategoryIndex : long { }
 public enum DocumentIndex : long { }
 public enum DescriptorIndex : long { }
+public enum ParameterIndex : long { }
 
 public class ParameterDescriptorData
 {
-    public string Name { get; set; }
+    public string Name { get; init; } = string.Empty;
 
     // Created from the BuiltInParameterGroup enumeration 
     // https://www.revitapidocs.com/2022/9942b791-2892-0658-303e-abf99675c5a6.htm
-    public string Group { get; set; }
+    public string Group { get; init; } = string.Empty;
     
     // The ID used to identify built-in parameters 
-    public int BuiltInParameterId { get; set; }
+    public int BuiltInParameterId { get; init; }
     
     // Shared Parameters have GUIDs
-    public string SharedParameterGuid { get; set; }
+    public string SharedParameterGuid { get; init; } = string.Empty;
     
     // Forge type ID
-    public string TypeId { get; set; } 
+    public string TypeId { get; init; } = string.Empty;
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Name, Group, BuiltInParameterId, SharedParameterGuid, TypeId);
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is ParameterDescriptorData pdd &&
+               Name.Equals(pdd.Name)
+               && Group.Equals(pdd.Group)
+               && BuiltInParameterId.Equals(pdd.BuiltInParameterId)
+               && SharedParameterGuid.Equals(pdd.SharedParameterGuid)
+               && TypeId.Equals(pdd.TypeId);
+    }
 }
 
 public class ElementData 
 {
-    // This is the so-called Unique ID in Revit, which is more stable than ElementId
-    public string Id { get; set; }
-    public string Name { get; set; }
+    // The ID is called a "Unique" id in the revit doce, but this does not always hold. 
+    // For example: the Existing Phase element's UniqueId has been observed to be shared between linked models 
+    // but we believe it to be intentional (so that we can link phases across models)
+    // More accurately the ID is a stable ID for use across models 
+    public string Id { get; set; } = string.Empty;
+
+    public string Name { get; set; } = string.Empty;
 
     // This is available only if the element is an ElementType. 
-    public string FamilyName { get; set; }
-    
+    public string FamilyName { get; set; } = string.Empty;
+
     public CategoryIndex Category { get; set; }
     public LevelIndex Level { get; set; }
     public ElementIndex Type { get; set; }
@@ -51,9 +71,19 @@ public class ElementData
 
 public class PointData
 {
-    public double X { get; set; }
-    public double Y { get; set; }
-    public double Z { get; set; }
+    public double X { get; init; }
+    public double Y { get; init; }
+    public double Z { get; init; }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(X, Y, Z);
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is PointData pd && X.Equals(pd.X) && Y.Equals(pd.Y) && Z.Equals(pd.Z);
+    }
 }
 
 public class ElementRef
@@ -63,15 +93,15 @@ public class ElementRef
 
 public class DocumentData : ElementRef
 {
-    public string PathName { get; set; }
-    public string Title { get; set; }
+    public string PathName { get; set; } = string.Empty;
+    public string Title { get; set; } = string.Empty;
     public bool IsLinked { get; set; }
     public bool IsDetached { get; set; }
 }
 
 public class RoomData : ElementRef
 {
-    public string Number { get; set; }
+    public string Number { get; set; } = string.Empty;
     public double BaseOffset { get; set; }
     public double LimitOffset { get; set; }
     public double UpperLimit { get; set; }
@@ -88,33 +118,27 @@ public class CategoryData
     public long Id { get; set; }
 }
 
+// TODO: we eventually want to separate this out into the four different classes,
+// One for each of the storage types: int, double, string (or unknown), ElementId
 public class ParameterData
 {
     public ElementIndex Element { get; set; } 
     public DescriptorIndex Descriptor { get; set; }
-    public string Value { get; set; }
+    public string Value { get; set; } = string.Empty;
 }
 
-public class RevitData
+/// <summary>
+/// In C# the Array class can only support up to 2 billion items however,
+/// we can go past if we roll our own List class. 
+/// </summary>
+public class BIMData
 {
-    public List<ParameterData> Parameters { get; set; } = new();
-    public List<ParameterDescriptorData> Descriptors { get; set; } = new();
-    public List<PointData> Points { get; set; } = new();
-    public List<CategoryData> Category { get; set; } = new();
-    public List<RoomData> Rooms { get; set; } = new();
-    public List<LevelData> Levels { get; set; } = new();
-    public List<DocumentData> Documents { get; set; } = new();
-    public List<ElementData> Elements { get; set; } = new();
-}
-
-public class RevitDataBuilder
-{
-    public RevitData RevitData { get; set; } = new();
-
-    public Dictionary<int, CategoryData> Categories = new();
-    public Dictionary<string, RoomData> Rooms = new();
-    public Dictionary<string, LevelData> Levels = new();
-    public Dictionary<string, DocumentData> Documents = new();
-    public Dictionary<string, ElementData> Elements = new();
-
+    public ParameterData[] Parameters { get; set; } = [];
+    public ParameterDescriptorData[] Descriptors { get; set; } = [];
+    public PointData[] Points { get; set; } = [];
+    public CategoryData[] Categories { get; set; } = [];
+    public RoomData[] Rooms { get; set; } = [];
+    public LevelData[] Levels { get; set; } = [];
+    public DocumentData[] Documents { get; set; } = [];
+    public ElementData[] Elements { get; set; } = [];
 }
